6565e68c580312a48b1140840997f416e4e984a3
harvey1344 <57689181+harvey1344@users.noreply.github.com>
2023-05-17 22:02:02 +0100

    new and improved name- no clue what changed

---
+++ nn.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
+
+# Load the ml-100k dataset
+header = ['user_id', 'item_id', 'rating', 'timestamp']
+df = pd.read_csv('./ml-100k/u.data', sep='\t', names=header)
+
+# Preprocess the dataset
+df['rating'] = df['rating'].astype(float)
+
+# Merge demographic information with the dataset
+user_info = pd.read_csv('./ml-100k/u.user', sep='|', names=['user_id', 'age', 'gender', 'occupation', 'zipcode'])
+df = df.merge(user_info, on='user_id')
+
+# Feature engineering
+user_features = df.groupby('user_id')['rating'].agg(['count', 'mean'])
+df = df.merge(user_features, how='left', on='user_id')
+
+# Convert gender and occupation to numerical labels
+df['gender'] = df['gender'].map({'M': 0, 'F': 1})
+df['occupation'] = df['occupation'].astype('category').cat.codes
+
+X = df[['user_id', 'item_id', 'count', 'mean', 'age', 'gender', 'occupation']].values
+y = df['rating'].values
+
+# Scale the features
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Define the deep neural network architecture
+model = Sequential()
+model.add(Dense(128, activation='relu', input_dim=X_train.shape[1]))
+model.add(Dropout(0.3))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(1, activation='linear'))
+
+# Compile the model
+model.compile(loss='mean_squared_error', optimizer=Adam())
+
+# Define early stopping callback
+early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+
+# Train the model
+model.fit(X_train, y_train, epochs=20, batch_size=32, validation_data=(X_test, y_test), callbacks=[early_stopping])
+
+# Evaluate the model
+rmse = np.sqrt(model.evaluate(X_test, y_test))
+print('Root Mean Squared Error:', rmse)
+
+
+
